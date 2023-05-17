96e255bbc049482303ed0a5ce03534487e78bd74
Ryo0333 <windsguitar@icloud.com>
2023-05-17 16:28:39 +0900

    ♻️ Linksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -3,6 +3,23 @@ import { Inter } from 'next/font/google'
 import styles from './Links.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const ITEMS = [{ href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+                 title: "Docs →",
+                 description: "Find in-depth information about Next.js features and&nbsp;API."
+                },
+                { href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+                 title: "Learn →",
+                 description: "Discover and deploy boilerplate example Next.js&nbsp;projects."
+                },
+                { href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+                 title: "Templates →",
+                 description: "Learn about Next.js in an interactive course with&nbsp;quizzes!  "
+                },
+                { href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+                 title: "Deploy →",
+                 description: "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel."
+                } 
+              ]
 
 export function Links() {
   return (
@@ -19,21 +36,25 @@ export function Links() {
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          {ITEMS.map(item => {
+            return (
+              <a key={item.href} href={item.href}
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={styles.title}>
-              Docs <span>-&gt;</span>
-            </h2>
+              {item.title}
+       </h2>
             <p className={styles.description}>
-              Find in-depth information about Next.js features and&nbsp;API.
+              {item.description}
             </p>
           </a>
+            );
+          })}
+          
 
-          <a
+          {/* <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -74,7 +95,7 @@ export function Links() {
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
     </div>
   )
