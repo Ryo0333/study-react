92b187565af0b0421ca945b4d7ae20ffe97cf90d
Ryo0333 <windsguitar@icloud.com>
2023-05-17 19:14:15 +0900

    ⚗️ クリックイベントの確認

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,16 +2,29 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { Main } from '@/components/Main'
 import { Header } from '@/components/Header'
+import Link from 'next/link'
+import { useCallback } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const foo =  1;
+
+  const  handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
+      <Link href='/about' onClick={handleClick}>
+        ボタン
+      </Link>
       <Main title="index" />
     </div>
   )
